f39cb57aa73b51f5648e2a1a1f07bdc46b061e99
badede <badede@gmail.com>
2017-06-16 19:01:42 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Read full dataset
+power_cons_data_all <- read.table("household_power_consumption.txt", header=TRUE, stringsAsFactors=FALSE, sep=";", na.strings="?")
+
+# Subset needed data
+power_cons_data_sub <- subset(power_cons_data_all, Date  %in% c("1/2/2007","2/2/2007"))
+rm("power_cons_data_all")
+
+# Plot 1
+png("plot1.png", height=480, width=480)
+attach(power_cons_data_sub)
+hist(Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+dev.off()
